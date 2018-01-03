d1c0c08ec737006d1a32bd7e2489a35d47417e17
latika18 <34892423+latika18@users.noreply.github.com>
2018-01-03 22:52:28 +0530

    Update Q001_basic_calculator.py

--- Q001_basic_calculator.py
+++ Q001_basic_calculator.py
@@ -1,4 +1,6 @@
-#calculator
+# Question : Create a simple function that given 3 inputs - number 1, number 2 and operator -  calculates the result.
+#The function supports DMAS operations, and accepts only integers
+
 def calculate(x , y , op):
     if op == '+' :
         z = x  + y
