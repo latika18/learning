7e5a75bc02739e7b8c905d9693efbd5b61b5b83f
latika18 <34892423+latika18@users.noreply.github.com>
2018-01-04 00:02:16 +0530

    Update and rename Q7.palindrome 3 digits to Q007_palindrome_from_product_of_3_digit_numbers.py

--- Q007_palindrome_from_product_of_3_digit_numbers.py
+++ Q007_palindrome_from_product_of_3_digit_numbers.py
@@ -1,5 +1,6 @@
+##A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 ##Find the largest palindrome made from the product of two 3-digit numbers.
-##Find the largest palindrome made from the product of two 4-digit numbers.
+##Find the largest palindrome made from the product of two 4-digit numbers.Link: https://projecteuler.net/problem=4
 
 def check_palindrome(s):
      if s == s[::-1]:
