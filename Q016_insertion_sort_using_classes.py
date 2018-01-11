2c9645c93efd3775577a0d45c7aba9a4a21d0d0d
latika18 <34892423+latika18@users.noreply.github.com>
2018-01-11 12:38:52 +0530

    Create Q016_insertion_sort_using_classes.py

---
+++ Q016_insertion_sort_using_classes.py
@@ -0,0 +1,15 @@
+class Node:
+   def __init__(self,item):
+       self.item = item
+       self.next = None
+       
+class Linkedlist ():
+    def __init__(self):
+        self.head = none
+        
+        
+if __name__= "__main__"
+    Llist = Linkedlist ()
+    Llist.head = Node (1)
+    
+    
