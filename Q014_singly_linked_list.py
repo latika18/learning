bac3475a263cf8f18bd5e40364670d647aa19e8d
latika18 <34892423+latika18@users.noreply.github.com>
2018-01-13 20:55:17 +0530

    Update Q014_singly_linked_list.py

--- Q014_singly_linked_list.py
+++ Q014_singly_linked_list.py
@@ -1,7 +1,10 @@
-
-
+##Question 14
+##Implement a singly linked list in python. You will need to define a class for a node, and create newer objects to hold new values.
+##Assume that your data will be like
+##>>> 98,52,45,19,37,22,1,66,943,415,21,785,12,698,26,36,18,97,0,63,25,85,24,94,1501
 
 class Node:
+
     def __init__(self,initdata):
         self.data = initdata
         self.next = None
@@ -21,9 +24,38 @@ class Node:
 
 class UnorderedList:
 
+
     def __init__(self):
         self.head = None
+
     def add(self,item):
         temp = Node(item)
         temp.setNext(self.head)
         self.head = temp
+
+    def printList(self):
+        current = self.head
+        count = 0
+        while current != None:
+            print current.getData()
+            current = current.getNext()
+            count = count +1
+
+
+temp = Node(67)
+print temp.getData()
+my_list = UnorderedList()
+my_list.add(93)
+my_list.add(60)
+my_list.add(34)
+my_list.add(9)
+my_list.add(5)
+my_list.add(193)
+my_list.add(30)
+my_list.add(54)
+my_list.add(707)
+my_list.add(930)
+my_list.add(600)
+my_list.add(1034)
+my_list.add(19)
+my_list.printList()
