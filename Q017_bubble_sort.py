f0846a07e3bd443c263860b5276caa3d08bbdf53
latika18 <34892423+latika18@users.noreply.github.com>
2018-01-20 23:08:16 +0530

    Update Q017_bubble_sort.py

--- Q017_bubble_sort.py
+++ Q017_bubble_sort.py
@@ -1,63 +1,40 @@
-##Question 15
-##Check if an element is present within a linked list or not.
-##Assume that your linked list data will be like
+# -*- coding: cp1252 -*-
+##Implement bubble sort in python. Don’t use Python’s built in sort or sorted.
+##Assume your inputs will be sufficient for the memory you have.
+##Example inputs
+##>>> 11,127,56,2,1,5,7,9,11,65,12,24,76,87,123,65,8,32,86,123,67,1,67,92,72,39,49,12
 ##>>> 98,52,45,19,37,22,1,66,943,415,21,785,12,698,26,36,18,97,0,63,25,85,24,94,1501
-##Ask for a number and check if it exists within the linked list
 
 
-class Node:
-    def __init__(self,item):
-        self.data = item
-        self.next = None
+def check_if_sorted(details):
+    """compares two consecutive integers in a list and returns the list if sorted else get the list sorted by calling another function"""
+    i = 0
+    while i <=  len(details) - 2:
+        if details[i] < details[i+1]:
+            status = True
+            i = i+1
+            print details
+        else:
+            bubble_sort(details)
+    if status :
+        return details
+        
 
-    def getData(self):
-        return self.data
+def bubble_sort(details):  
+    """compares two consecutive integers in a list and shifts the smaller one to left """
+    for i in range(len(details)-1):
+        if details[i] > details[i+1]:
+            temp = details[i]
+            details[i]= details[i+1]
+            details[i+1] = temp
+    return check_if_sorted(details)
+        
+        
+sort_me = [11,127,56,2,1,5,7,9,11,65,12,24,76,87,123,65,8,32,86,123,67,1,67,92,72,39,49,12, 98,52,45,19,37,22,1,66,943,415,21,785,12,698,26,36,18,97,0,63,25,85,24,94,1501]
+print sort_me 
+print bubble_sort(sort_me)
 
-    def getNext(self):
-        return self.next
-
-    def setData(self,newdata):
-        self.data = newdata
-
-    def setNext(self, newnext):
-        self.next = newnext
-
-
-class Linkedlist:
-    def __init__(self):
-        self.head = None
-
-    def add_member(self,data):
-        temp = Node(data)
-        temp.setNext(self.head)
-        self.head = temp
-
-    def search_member(self,item):
-        current = self.head
-        while current != None:
-            if current.getData() ==item:
-                return True
-            else:
-                current =current.getNext()
-        return False
-
-llist = Linkedlist()
-llist.add_member(23)
-llist.add_member(98)
-llist.add_member(415)
-llist.add_member(123)
-llist.add_member(981)
-llist.add_member(454)
-llist.add_member(213)
-llist.add_member(198)
-llist.add_member(455)
-llist.add_member(253)
-llist.add_member(978)
-llist.add_member(45)
-llist.add_member(203)
-llist.add_member(918)
-llist.add_member(45)
-item = int(raw_input("Enter the number you want to search : "))
-print llist.search_member(item)
-
+            
+        
+                
 
