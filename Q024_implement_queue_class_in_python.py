423a5bc56cc4b3bf7e4b0238486d6cec0a90ffcc
latika18 <34892423+latika18@users.noreply.github.com>
2018-01-21 21:32:50 +0530

    Create Q024_implement_queue_class_in_python.py

---
+++ Q024_implement_queue_class_in_python.py
@@ -0,0 +1,5 @@
+Question 24
+Implement a queue class in Python: It should support 3 APIs:
+queue.top(): prints current element at front of queue
+queue.pop(): takes out an element from front of queue
+queue.add(): adds a new element at end of stack
