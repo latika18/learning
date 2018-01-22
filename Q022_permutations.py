18f35f09a7b60643461c69c12861be154d3e7706
latika18 <34892423+latika18@users.noreply.github.com>
2018-01-22 22:45:42 +0530

    Update Q022_permutations.py

--- Q022_permutations.py
+++ Q022_permutations.py
@@ -1,34 +1,35 @@
-###Problem : Given a list of strings, some of which may be anagrams amongst themselves, print the
-###permutations which can be so constructed so that each permutation has set of strings which
-###is completely unique.
-###Example input:
-###>>> “asda”, “daas”, “dand”, “nadd”
-###{“asda”, “dand”}, {“daas”, “dand”}, {“asda”, “nadd”}, {“daas”, “nadd”},
-###>>> “laptop”, “toplap”, “loptap”, “mouse”
-###{“laptop”, “mouse”}, {“toplap”, “mouse”}, {“loptap”, “mouse”}
-def anagram(value):
-    '''Prints random anagram of given value'''
-    import random
-    newWord = ''
-    for i in range(len(value)):
-        pos = random.randint(0, len(value)-1)
-        newWord += value[pos]
-        value = value[:pos] + value[pos+1:]
-    return newWord
+# -*- coding: cp1252 -*-
+##Given a list of strings, some of which may be anagrams amongst themselves, print the
+##permutations which can be so constructed so that each permutation has set of strings which
+##is completely unique.
+##Example input:
+##>>> “asda”, “daas”, “dand”, “nadd”
+##{“asda”, “dand”}, {“daas”, “dand”}, {“asda”, “nadd”}, {“daas”, “nadd”},
+##>>> “laptop”, “toplap”, “loptap”, “mouse”
+##{“laptop”, “mouse”}, {“toplap”, “mouse”}, {“loptap”, “mouse”}
 
+
+def is_anagram(value_1, value_2):
+    """checks whether the two given strings are anagrams """
+    if sorted(value_1) == sorted(value_2):
+        return True
+        
 def random_set(input_value):
+    """returns the set of strings which are completely unique"""
     if len(input_value) <= 2:
         output = input_value
     else:
-        for i in range(len(input_value) - 1):
-            for j in range(1,len(input_value) - 1):
-                print i , j
-                print anagram(input_value[j])
-                if input_value[i] != anagram(input_value[j]):
-                    output = {input_value[i] ,input_value[j]}
-
+        output = []
+        for i in range(len(input_value)):
+            for j in range(len(input_value)):
+                if not is_anagram(input_value[i], input_value[j]):
+                    output.append( {input_value[i], input_value[j]})
 
     return output
-    
-print random_set(["asda", "daas" , "dand" , "naad"])
+
+print random_set(["asda", "daas" , "dand" , "nadd"])
+print random_set(["laptop", "toplap", "loptap", "mouse"])
+
+
+
 
