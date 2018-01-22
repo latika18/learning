c5ed37420c0f8206cb85b0b36d253dbb6554dbf7
latika18 <34892423+latika18@users.noreply.github.com>
2018-01-22 22:48:00 +0530

    Update Q023_implement_a_stack_class_in_python.py

--- Q023_implement_a_stack_class_in_python.py
+++ Q023_implement_a_stack_class_in_python.py
@@ -1,5 +1,5 @@
-Question 23
-Implement a stack class in Python. It should support 3 APIs:
-stack.top(): prints element at top of stack
-stack.pop(): takes out an element from top of stack
-stack.push(): adds a new element at top of stack
+###Question 23
+###Implement a stack class in Python. It should support 3 APIs:
+###stack.top(): prints element at top of stack
+###stack.pop(): takes out an element from top of stack
+###stack.push(): adds a new element at top of stack
