57cae9109f01b19682fdec7358893d376a9c251f
latika18 <34892423+latika18@users.noreply.github.com>
2018-01-22 17:13:18 +0530

    Update Q022_permutations.py

--- Q022_permutations.py
+++ Q022_permutations.py
@@ -1,8 +1,8 @@
-Given a list of strings, some of which may be anagrams amongst themselves, print the
-permutations which can be so constructed so that each permutation has set of strings which
-is completely unique.
-Example input:
->>> “asda”, “daas”, “dand”, “nadd”
-{“asda”, “dand”}, {“daas”, “dand”}, {“asda”, “nadd”}, {“daas”, “nadd”},
->>> “laptop”, “toplap”, “loptap”, “mouse”
-{“laptop”, “mouse”}, {“toplap”, “mouse”}, {“loptap”, “mouse”}
+###Problem : Given a list of strings, some of which may be anagrams amongst themselves, print the
+###permutations which can be so constructed so that each permutation has set of strings which
+###is completely unique.
+###Example input:
+###>>> “asda”, “daas”, “dand”, “nadd”
+###{“asda”, “dand”}, {“daas”, “dand”}, {“asda”, “nadd”}, {“daas”, “nadd”},
+###>>> “laptop”, “toplap”, “loptap”, “mouse”
+###{“laptop”, “mouse”}, {“toplap”, “mouse”}, {“loptap”, “mouse”}
