2f645d57c55090e288a9fd084c03c1d3a1df8811
latika18 <34892423+latika18@users.noreply.github.com>
2018-02-18 21:43:44 +0530

    Update Q025_to_do_binary_search_tree_of_a_given_array.py

--- Q025_to_do_binary_search_tree_of_a_given_array.py
+++ Q025_to_do_binary_search_tree_of_a_given_array.py
@@ -6,61 +6,110 @@
 
 
  
-# A node structure
-class Node:
-    # A utility function to create a new node
-    def __init__(self, key):
-        self.key = key
+class Tree(object):
+
+    def __init__(self):
+
+        self.root = Node()
+
+    def insert(self,data):
+
+        self.root.insert(data)
+
+    def delete(self,data):
+
+        self.root.delete(data)
+
+    def print_elements(self):
+
+        return self.root.print_element()
+
+class Node(object):
+
+    def __init__(self,data = None, parent = None):
+
+        self.parent = parent
+        self.data = data
         self.left = None
         self.right = None
- 
 
-def binary_search_tree(input_array):
-    """creates a binary search tree representation of a given array"""
-   
-    binary_tree = []
-    
-    #Base case when array is empty
-    if (len(input_array)) == 1:
-        return binary_tree 
-    else :
-        print binary_tree
-        root = Node(input_array[0])
-        input_array.pop(0)
-        binary_tree.append(root)
-        
-        if root >= input_array[0]:
-            
-            root.left = input_array[0]
-            input_array.pop(0)
-            binary_tree.append(root.left)
-            for j in range(len(input_array)-1):
-                print binary_tree 
-                if root <= input_array[j]:
-                    root.right = input_array[j]
-                    input_array.pop(j)
-                    binary_tree.append(root.right)
-                    print binary_tree 
-                    break
-        elif root <= input_array[0]:
-            print binary_tree 
-            root.right = input_array[0]
-            input_array.pop(0)
-            binary_tree.append(root.right)
-            for j in range(len(input_array)-1):
-                if root >= input_array[j]:
-                    root.left = input_array[j]
-                    input_array.pop(j)
-                    binary_tree.append(root.left)
-                    print binary_tree 
-                    break   
-           
-        return binary_search_tree(input_array)
+    def insert(self,data):
+
+        if self.data == None:
+
+            self.data = data
+        else:
+            if data < self.data:
+                if self.left == None:
+                    self.left = Node(data,self)
+                else:
+                    self.left.insert(data)
+            else:
+                if data > self.data:
+                    if self.right == None:
+                        self.right = Node(data,self)
+                    else:
+                        self.right.insert(data)
+
+    def delete(self,data):
+
+        if self.data == data:
+
+            if self.right == None and self.left == None:        
+
+                if self.parent.left.data == data:
+
+                    self.parent.left = None
+                else:
+
+                    self.parent.right = None
+            elif self.right == None or self.left == None:
+
+                if self.right == None:
+
+                    self.data , self.left = self.left.data , self.left.left
+
+                else:
+
+                    self.data, self.right = self.right.data , self.right.right
+
+            else:
+
+                counter = self.right
+                while True:
+                    if counter.right == None:
+                        break
+                    else:
+                        counter = counter.left
+                self.data = counter.data
+                counter.delete(counter.data)
+
+        else:
+            if data < self.data:
+                self.left.delete(data)
+            else:
+                self.right.delete(data)
 
 
+    def search(self,data):
 
-array = [100,12,98,223,-78,-12,82,96,74,30,789,912,120]
-print binary_search_tree(array)
+        if self.data == data:
+            return self.data
+        else:
+            if data < self.data:
+                return self.left.search(data)
+            else:
+                return self.right.search(data)
 
+    def print_element(self):
 
-        
+        if self.parent:
+
+            print self.data, 'is below %s' % (self.parent.data)
+        else:
+            print self.data
+
+        if self.right != None:
+            self.right.print_element()
+        if self.left != None:
+            self.left.print_element()
