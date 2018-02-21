ad859f5bd08c150306c47d327840302157b6d9a4
latika18 <34892423+latika18@users.noreply.github.com>
2018-02-21 22:29:45 +0530

    Update Q002_check_if_prime_using_classes.py

--- Q002_check_if_prime_using_classes.py
+++ Q002_check_if_prime_using_classes.py
@@ -1,1 +1,31 @@
 
+#implement a class prime
+class prime():
+    def __init__(self,num,k):
+        self.num = int(num)
+        self.k = k
+
+    def check_prime(self):
+        
+        for i in range (2,self.num/2):
+            if self.num % i:
+                self.k =1
+                break
+            
+    def show_result(self):
+        if self.k == 1:
+            print "num is not prime"
+        else:
+            print "num is prime"
+            
+##main program
+
+#a = int(raw_input("Enter a number : "))
+a = prime(raw_input("Enter a number : ") ,0)
+
+a.check_prime()
+a.show_result()     
+
+
+
+
