7baf471e3a6cc9fd46a5f08e4a00e33be57723a1
latika18 <34892423+latika18@users.noreply.github.com>
2018-04-01 23:19:05 +0530

    Update Q025_to_do_binary_search_tree_of_a_given_array.py

--- Q025_to_do_binary_search_tree_of_a_given_array.py
+++ Q025_to_do_binary_search_tree_of_a_given_array.py
@@ -3,113 +3,30 @@
 ##Example Input:
 ##100,12,98,223,-78,-12,82,96,74,30,789,912,120
 
+class Node(object):
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
 
+class BST(object):
+    def __init__(self, val):
+        self.root = Node(val)
+
+    def insert(self, value):
+        current = self.root
+        while current:
+            if value < current.value:
+                if current.left == None:
+                    current.left = Node(value)
+                    break
+                else:
+                    current = current.left
+            else:
+                if current.right == None:
+                    current.right = Node(value)
+                    break
+                else:
+                    current = current.right
 
  
-class Tree(object):
-
-    def __init__(self):
-
-        self.root = Node()
-
-    def insert(self,data):
-
-        self.root.insert(data)
-
-    def delete(self,data):
-
-        self.root.delete(data)
-
-    def print_elements(self):
-
-        return self.root.print_element()
-
-class Node(object):
-
-    def __init__(self,data = None, parent = None):
-
-        self.parent = parent
-        self.data = data
-        self.left = None
-        self.right = None
-
-    def insert(self,data):
-
-        if self.data == None:
-
-            self.data = data
-        else:
-            if data < self.data:
-                if self.left == None:
-                    self.left = Node(data,self)
-                else:
-                    self.left.insert(data)
-            else:
-                if data > self.data:
-                    if self.right == None:
-                        self.right = Node(data,self)
-                    else:
-                        self.right.insert(data)
-
-    def delete(self,data):
-
-        if self.data == data:
-
-            if self.right == None and self.left == None:        
-
-                if self.parent.left.data == data:
-
-                    self.parent.left = None
-                else:
-
-                    self.parent.right = None
-            elif self.right == None or self.left == None:
-
-                if self.right == None:
-
-                    self.data , self.left = self.left.data , self.left.left
-
-                else:
-
-                    self.data, self.right = self.right.data , self.right.right
-
-            else:
-
-                counter = self.right
-                while True:
-                    if counter.right == None:
-                        break
-                    else:
-                        counter = counter.left
-                self.data = counter.data
-                counter.delete(counter.data)
-
-        else:
-            if data < self.data:
-                self.left.delete(data)
-            else:
-                self.right.delete(data)
-
-
-    def search(self,data):
-
-        if self.data == data:
-            return self.data
-        else:
-            if data < self.data:
-                return self.left.search(data)
-            else:
-                return self.right.search(data)
-
-    def print_element(self):
-
-        if self.parent:
-
-            print self.data, 'is below %s' % (self.parent.data)
-        else:
-            print self.data
-
-        if self.right != None:
-            self.right.print_element()
-        if self.left != None:
-            self.left.print_element()
