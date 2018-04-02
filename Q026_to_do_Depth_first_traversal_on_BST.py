7c0be60f53e9325684bffb87f7a4d9a22a120b1e
latika18 <34892423+latika18@users.noreply.github.com>
2018-04-02 20:30:52 +0530

    Update Q026_to_do_Depth_first_traversal_on_BST.py

--- Q026_to_do_Depth_first_traversal_on_BST.py
+++ Q026_to_do_Depth_first_traversal_on_BST.py
@@ -2,55 +2,47 @@
 ###Example Input:
 ###100,12,98,223,-78,-12,82,96,74,30,789,912,120
 # A node structure
-class Node:
-    # A utility function to create a new node
-    def __init__(self, key):
-        self.key = key
-        self.left = None
-        self.right = None
+class Node(object):
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+class BST(object):
+    def __init__(self, val):
+        self.root = Node(val)
+
+    def insert(self, value):
+        current = self.root
+        while current:
+            if value < current.value:
+                if current.left == None:
+                    current.left = Node(value)
+                    break
+                else:
+                    current = current.left
+            else:
+                if current.right == None:
+                    current.right = Node(value)
+                    break
+                else:
+                    current = current.right
+    
+       
+
  
 
-def binary_search_tree(input_array):
-    """creates a binary search tree representation of a given array"""
-   
-    binary_tree = []
-    
-    #Base case when array is empty
-    if (len(input_array)) == 1:
-        return binary_tree 
-    else :
-        print binary_tree
-        root = Node(input_array[0])
-        input_array.pop(0)
-        binary_tree.append(root)
-        
-        if root >= input_array[0]:
-            
-            root.left = input_array[0]
-            input_array.pop(0)
-            binary_tree.append(root.left)
-            for j in range(len(input_array)-1):
-                print binary_tree 
-                if root <= input_array[j]:
-                    root.right = input_array[j]
-                    input_array.pop(j)
-                    binary_tree.append(root.right)
-                    print binary_tree 
-                    break
-        elif root <= input_array[0]:
-            print binary_tree 
-            root.right = input_array[0]
-            input_array.pop(0)
-            binary_tree.append(root.right)
-            for j in range(len(input_array)-1):
-                if root >= input_array[j]:
-                    root.left = input_array[j]
-                    input_array.pop(j)
-                    binary_tree.append(root.left)
-                    print binary_tree 
-                    break   
-           
-        return binary_search_tree(input_array)
+t = BST(22)
+t.insert(30)
+t.insert(10)
+t.insert(80)
+t.insert(90)
+t.insert(9)
+t.insert(23)
+t.insert(14)
+t.insert(6)
+t.insert(40)
+t.insert(60)
+t.insert(3)
 
-
-
+             
