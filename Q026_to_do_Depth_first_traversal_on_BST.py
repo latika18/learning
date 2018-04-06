2bde75dbdc8599c967af94cd424804044065825b
latika18 <34892423+latika18@users.noreply.github.com>
2018-04-06 21:21:56 +0530

    Update Q026_to_do_Depth_first_traversal_on_BST.py

--- Q026_to_do_Depth_first_traversal_on_BST.py
+++ Q026_to_do_Depth_first_traversal_on_BST.py
@@ -28,41 +28,39 @@ class BST(object):
                 else:
                     current = current.right
                     
-    def inorder(self,root):
-        ###left , root , right
-##        import pdb;pdb.set_trace()
-        if root:
-            print root.value
-            if root.left:
-                self.inorder(root.left)
-            if root.right:
-                self.inorder(root.right)
-                
-
-
-     def preorder(self,root):
+     
+    
+    def preorder(self,root):
 ##        root,left,right
         
       if root:
-            if root.left:
+          print root.value
+          if root.left:
                 self.preorder(root.left)
-                print root
-            if root.right:
+          if root.right:
                 self.preorder(root.right)
      
 
     def postorder(self,root):
 ##      left ,right , root
 ##        import pdb;pdb.set_trace()
-            
+        if root:
+            if root.left:
+                self.postorder(root.left)
+            if root.right:
+                self.postorder(root.right)
+            print root.value
+
+
+    def inorder(self,root):
+##        leftt,root,right
+##        import pdb;pdb.set_trace()
+          
         if root.left:
-            self.postorder(root.left)
-        print root.left
-        if root.right:
-            self.postorder(root.right)
+            self.inorder(root.left)
         print root.value
-
-
+        if root.right:
+            self.inorder(root.right)
 
 
 t = BST(22)
