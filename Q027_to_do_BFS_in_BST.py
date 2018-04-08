92e8dc9fc3ebe8f780592205ec2de01f0d02d31b
latika18 <34892423+latika18@users.noreply.github.com>
2018-04-08 21:29:44 +0530

    Update Q027_to_do_BFS_in_BST.py

--- Q027_to_do_BFS_in_BST.py
+++ Q027_to_do_BFS_in_BST.py
@@ -1,1 +1,26 @@
 # Question 27 Perform breadth first traversal on a BST and print the elements traversed
+class Node(object):
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+class BST(object):
+    def __init__(self, value):
+        self.root = Node(value)
+
+    def insert(self, value):
+        current = self.root
+        while current:
+            if value > current.value:
+                if current.right == None:
+                    current.right  = Node(value)
+                    break
+                else:
+                    current = current.right
+            else:
+                if current.left == None :
+                    current.left  = Node(value)
+                    break
+                else:
+                    current = current.left
