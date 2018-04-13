98ab0546b882bb14102b04dc33a2ee26495193c0
latika18 <34892423+latika18@users.noreply.github.com>
2018-04-13 20:25:17 +0530

    Update Q027_to_do_BFS_in_BST.py

--- Q027_to_do_BFS_in_BST.py
+++ Q027_to_do_BFS_in_BST.py
@@ -24,3 +24,45 @@ class BST(object):
                     break
                 else:
                     current = current.left
+     def Breadth_first_search(self,root):
+        """In BFS the Node Values at each level of the Tree are traversed before going to next level"""
+
+        visited = []
+        if root:
+            visited.append(root)
+            print root.value
+        current = root
+        while current :
+            if current.left:
+                print current.left.value
+                visited.append(current.left)
+            if current.right:
+                print current.right.value
+                visited.append(current.right)
+            visited.pop(0)
+            current = visited[0]
+        
+               
+  
+t = BinarySearchTree(100)
+t.insert(12)
+t.insert(92)
+t.insert(112)
+t.insert(123)
+t.insert(2)
+t.insert(11)
+t.insert(52)
+t.insert(3)
+t.insert(66)
+t.insert(10)
+
+print "Output of Breadth First search is "
+t.Breadth_first_search(t.root)
+
+
+
+
+
+
+
+                   
