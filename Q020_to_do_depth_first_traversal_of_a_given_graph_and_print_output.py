0aab1fcdc4808f0aaa7064c88a653d9b45c906ec
latika18 <34892423+latika18@users.noreply.github.com>
2018-04-15 21:42:51 +0530

    Update Q020_to_do_depth_first_traversal_of_a_given_graph_and_print_output.py

--- Q020_to_do_depth_first_traversal_of_a_given_graph_and_print_output.py
+++ Q020_to_do_depth_first_traversal_of_a_given_graph_and_print_output.py
@@ -8,4 +8,39 @@ Example input:
 3 -> 5
 4 -> 2
 #consttructor
-class graph(object)
+from collections import defaultdict
+import pdb
+class Graph(object):
+    def __init__(self):
+        self.graph = defaultdict(list)
+
+    def add_edge(self, u, v):
+        self.graph[u].append(v)
+
+    def depth_first_search(self,node):
+        visited = [False] * len(self.graph)
+        stack = [node]
+        visited[node] = True
+        while stack:
+            
+            node = stack.pop()
+            print node
+            for i in self.graph[node]:
+                pdb.set_trace()
+                print i
+                if not i:
+                    break
+                
+                   
+                if visited[i] == False:
+                    visited[i] = True
+                    stack.append(i)
+
+G = Graph()
+G.add_edge(1,2)
+G.add_edge(1,4)
+G.add_edge(1,5)
+G.add_edge(2,3)
+G.add_edge(3,5)
+G.depth_first_search(1)
+
