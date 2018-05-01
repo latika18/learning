bd8e2e7553dca87f32dfc639fceabd993e0b7672
latika18 <34892423+latika18@users.noreply.github.com>
2018-05-02 01:09:16 +0530

    Update Q015_search_num_in_given_linkedlist_c.py

--- Q015_search_num_in_given_linkedlist_c.py
+++ Q015_search_num_in_given_linkedlist_c.py
@@ -5,63 +5,109 @@
 
 
 
-class Node:
-    
-    
-    def __init__(self, data , next = None):
+class Node(object):
+    def __init__(self,data, next=None):
         self.data = data
         self.next = next
 
-##    def getData(self):
-##        return self.data
-##
-##    def getNext(self):
-##        return self.next
-##
-##    def setData(self,newdata):
-##        self.data = newdata
-##
-##    def setNext(self, newnext):
-##        self.next = newnext
+class LinkedList(object):
 
+    def __init__(self):
+        self.head = None
+        self.size =0
+    
 
-class Linkedlist:
-    def __init__(self,seq = None):
-        self.seq = seq
-        if seq != None:
-            self.extend(seq)
+    def extend(self, seq = None):
+        """extends list with the given sequence"""
+        
+        for i in range(0, len(seq)):
+            node = Node(seq[i])
+            self.size +=1
+            node.next = self.head
+            self.head = node
 
-##    def __contains__(self, item):
-##        for i in self:
-##        ...
+    def append(self, item):
+        """append item to the end of list"""
+        node = Node(item)
+        node.next = self.head
+        self.head = node
+        self.size += 1
+                
+    def printdata(self):
+        """print elements of linked list"""
+        node = self.head 
+        while node:
+            print node.data
+            node = node.next
 
     def __iter__(self):
         node = self.head
         while node:
-            yield node.value
+            yield node.data
             node = node.next
 
-    def add_member(self,data):
-        temp = Node(data)
-        temp.setNext(self.head)
-        self.head = temp
-
-    def search_member(self,item):
-        current = self.head
-        while current != None:
-            if current.getData() ==item:
+    def __contains__(self, item):
+        """checks whether given item in list"""
+        node = self.head 
+        while node:
+            if node.data == item:
                 return True
-            else:
-                current =current.getNext()
-        return False
+            node = node.next
+                
+
+    def len(self):
+        """returns the length of list"""
+        return self.size
+    
+   
+    def remove(self, item):
+        """removes item from list"""
+        node = self.head
+        current = self.head.next
+        if node.data == item:
+            self.size -= 1
+            self.head = current
+            current = current.next
+        while current:
+            if current.data == item:
+                current = current.next
+                node.next = current
+                
+                self.size -= 1
+            node = current
+            current = current.next
+
+        
+        
+                
+    def __str__(self):
+        return  str(self.data) + str(self.size)
 
 
-llist = Linkedlist([23, 98, 415, 123, 981, 454, 213, 198])
 
-while True:
-    item = int(raw_input("Enter a number to search for: "))
+if __name__ == "__main__":
+    
+    llist = LinkedList()
+    llist.extend([98,52,45,19,37,22,1,66,943,415,21,785,12,698,26,36,18,97,0,63,25,85,24])
 
-    if item in llist:
-        print "It's in there!"
-    else:
-        print "Sorry, don't have that one . Check for another number."
+    print "Length of linked list is ", llist.len()
+    
+    llist.append(222)
+        
+    print "Length of linked list is ", llist.len()
+
+    llist.remove(22)
+        
+    print "Elements of linked list  \n", llist.printdata()
+    print "Length of linked list is ", llist.len()
+    
+   ## Search for an element in list   
+    while True:
+        item = int(raw_input("Enter a number to search for: "))
+        if item in llist:
+            print "It's in there!"
+        else:
+            print "Sorry, don't have that one"
+
+       
+
