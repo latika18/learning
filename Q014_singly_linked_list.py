858b512a5ff14135dc86134de27a9306338d1ac7
latika18 <34892423+latika18@users.noreply.github.com>
2018-05-02 01:08:07 +0530

    Update Q014_singly_linked_list.py

--- Q014_singly_linked_list.py
+++ Q014_singly_linked_list.py
@@ -3,34 +3,113 @@
 ##Assume that your data will be like
 ##>>> 98,52,45,19,37,22,1,66,943,415,21,785,12,698,26,36,18,97,0,63,25,85,24,94,1501
 
-import pdb
 class Node(object):
-
-    def __init__(self,data=None, next=None):
+    def __init__(self,data, next=None):
         self.data = data
         self.next = next
 
 class LinkedList(object):
 
-    def __init__(self, seq=None):
-        self.head = Node()
+    def __init__(self):
+        self.head = None
         self.size =0
-        if seq is not None:
-            self.extend(seq)
-      
+    
 
-    def extend(self, seq):
+    def extend(self, seq = None):
         """extends list with the given sequence"""
-        node = self.head
+        
         for i in range(0, len(seq)):
-            node = Node(seq[i],node.next)
-            print node.data
+            node = Node(seq[i])
             self.size +=1
-            node.next = node
+            node.next = self.head
+            self.head = node
 
-    def append(self, a):
-        """append string a to the end of list"""
+    def append(self, item):
+        """append item to the end of list"""
+        node = Node(item)
+        node.next = self.head
+        self.head = node
+        self.size += 1
+                
+    def printdata(self):
+        """print elements of linked list"""
+        node = self.head 
+        while node:
+            print node.data
+            node = node.next
+
+    def __iter__(self):
         node = self.head
+        while node:
+            yield node.data
+            node = node.next
+
+    def __contains__(self, item):
+        """checks whether given item in list"""
+        node = self.head 
+        while node:
+            if node.data == item:
+                return True
+            node = node.next
+                
+
+    def len(self):
+        """returns the length of list"""
+        return self.size
+    
+   
+    def remove(self, item):
+        """removes item from list"""
+        node = self.head
+        current = self.head.next
+        if node.data == item:
+            self.size -= 1
+            self.head = current
+            current = current.next
+        while current:
+            if current.data == item:
+                current = current.next
+                node.next = current
+                
+                self.size -= 1
+            node = current
+            current = current.next
+
+        
+        
+                
+    def __str__(self):
+        return  str(self.data) + str(self.size)
+
+
+
+if __name__ == "__main__":
+    
+    llist = LinkedList()
+    llist.extend([98,52,45,19,37,22,1,66,943,415,21,785,12,698,26,36,18,97,0,63,25,85,24])
+
+    print "Length of linked list is ", llist.len()
+    
+    llist.append(222)
+        
+    print "Length of linked list is ", llist.len()
+
+    llist.remove(22)
+        
+    print "Elements of linked list  \n", llist.printdata()
+    print "Length of linked list is ", llist.len()
+    
+   ## Search for an element in list   
+    while True:
+        item = int(raw_input("Enter a number to search for: "))
+        if item in llist:
+            print "It's in there!"
+        else:
+            print "Sorry, don't have that one"
+
+       
+
+
         while node:
             if node.next == None:
                 node = Node(a,node.next)
