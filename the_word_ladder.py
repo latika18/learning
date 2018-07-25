2b06327e2f82e1310c91f738866732d3f6d9c602
latika18 <34892423+latika18@users.noreply.github.com>
2018-07-25 21:17:17 +0530

    Update the_word_ladder.py

--- the_word_ladder.py
+++ the_word_ladder.py
@@ -1,1 +1,5 @@
-
+Our first problem is to figure out how to turn a large collection of words into a graph. 
+What we would like is to have an edge from one word to another if the two words are only different by a single letter. 
+If we can create such a graph, then any path from one word to another is a solution to the word ladder puzzle. 
+Figure 1 shows a small graph of some words that solve the 
+FOOL to SAGE word ladder problem. Notice that the graph is an undirected graph and that the edges are unweighted.
