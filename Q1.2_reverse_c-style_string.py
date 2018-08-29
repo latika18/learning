30efa2385aabd61ec25b89a6d5dc4f2b57966a84
latika18 <34892423+latika18@users.noreply.github.com>
2018-08-29 22:09:57 +0530

    Update Q1.2_reverse_c-style_string.py

--- Q1.2_reverse_c-style_string.py
+++ Q1.2_reverse_c-style_string.py
@@ -1,1 +1,7 @@
 1.2 Write code to reverse a C-Style String. (C-String means that “abcd” is represented as five characters, including the null character.)
+
+def reverse_string(string1):
+    string1=string1[::-1]
+    return string1[1:]
+
+print reverse_string('1234 ')
