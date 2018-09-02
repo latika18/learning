0fc3f8ed109681f951b2c3ca614e350e3caec241
latika18 <34892423+latika18@users.noreply.github.com>
2018-09-02 11:02:37 +0530

    Create Q1.4_is_anagram.py

---
+++ Q1.4_is_anagram.py
@@ -0,0 +1,22 @@
+##Write a method to decide if two strings are anagrams or not
+
+def is_anagram(wd1,wd2):
+    for c in wd1:
+        wd1= wd1.replace(c,"")
+        while wd1:
+            if c in wd2:
+                wd2 = wd2.replace(c, "")
+                print c , wd1, wd2
+            else:
+                return False
+    print len(wd2)
+    if len(wd2)== 0:
+        return True
+    else:
+        return False
+
+
+print is_anagram('xxxx','xxx')
+    
+
+
