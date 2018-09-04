54d64658060239c4cc23df099fc923935b40db50
latika18 <34892423+latika18@users.noreply.github.com>
2018-09-04 21:42:28 +0530

    Update Q1.5_replace_space.py

--- Q1.5_replace_space.py
+++ Q1.5_replace_space.py
@@ -1,8 +1,9 @@
 #Q1.5 Write a method to replace all space in a string with "%20"
 
 def replace_space(string1):
-    if c == " ":
-        string1.replace(c,"%20")
+    for c in string1:
+        if c == " ":
+            string1 = string1.replace(c,"%20")
     return string1
 
 print replace_space("This is a plain text")
