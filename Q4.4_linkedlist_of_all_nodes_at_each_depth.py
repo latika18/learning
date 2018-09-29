009240e56fd1e0828e64eca535b7cbb0edfdae58
latika18 <34892423+latika18@users.noreply.github.com>
2018-09-30 04:56:07 +0530

    Create Q4.4_linkedlist_of_all_nodes_at_each_depth.py

---
+++ Q4.4_linkedlist_of_all_nodes_at_each_depth.py
@@ -0,0 +1,4 @@
+Given a binary search tree, design an algorithm which creates a linked list of all the nodes at each depth
+(i.e., if you have a tree with depth D, you’ll have D linked lists).
+_
+________________________________________________________________pg 126
