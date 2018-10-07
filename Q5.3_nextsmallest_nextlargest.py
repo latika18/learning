9d7d5e9b8799113ad25fa85b0fcdd083c9eecccd
latika18 <34892423+latika18@users.noreply.github.com>
2018-10-07 13:59:01 +0530

    Create Q5.3_nextsmallest_nextlargest.py

---
+++ Q5.3_nextsmallest_nextlargest.py
@@ -0,0 +1,3 @@
+Given an integer, print the next smallest and next largest number that have the same number of 1 bits in their binary representation.
+_
+_______________________________________________________
