82cae724ed053502bfbb1659b80ff3ae5eab9358
latika18 <34892423+latika18@users.noreply.github.com>
2018-10-17 20:57:32 +0530

    Update Q4.8_print_all_paths_sum.py

--- Q4.8_print_all_paths_sum.py
+++ Q4.8_print_all_paths_sum.py
@@ -1,4 +1,5 @@
-You are given a binary tree in which each node contains a value. Design an algorithm to print all paths which sum up to that value. 
+You are given a binary tree in which each node contains a value.
+Design an algorithm to print all paths which sum up to that value. 
 Note that it can be any path in the tree - it does not have to start at the root.
 _
 ________________________________________________________________
