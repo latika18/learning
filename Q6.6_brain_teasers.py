44792ef072d8626002b9c825aa0e564c9638efda
latika18 <34892423+latika18@users.noreply.github.com>
2018-10-30 22:24:15 -0700

    Update Q6.6_brain_teasers.py

--- Q6.6_brain_teasers.py
+++ Q6.6_brain_teasers.py
@@ -1,1 +1,6 @@
 
+6.6 There are one hundred closed lockers in a hallway. A man begins by opening all one hundred lockers. 
+Next, he closes every second locker. Then he goes to every third locker and closes it if it is open or
+opens it if it is closed (e.g., he toggles every third locker). 
+After his one hundredth pass in the hallway,
+in which he toggles only locker number one hundred, how many lockers are open? 
