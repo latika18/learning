e2b9bbc00068179de735d6065bef9ba03513d667
latika18 <34892423+latika18@users.noreply.github.com>
2018-11-06 21:52:51 +0530

    Update Q7.1_deck_of_cards.py

--- Q7.1_deck_of_cards.py
+++ Q7.1_deck_of_cards.py
@@ -1,2 +1,2 @@
-7.1 Design the data structures for a generic deck of cards.
-Explain how you would subclass it to implement particular card games. 
+#7.1 Design the data structures for a generic deck of cards.
+#Explain how you would subclass it to implement particular card games. 
