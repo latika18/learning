d9fbc976bb8f15bc4828c17dcec9d3cd48e04469
latika18 <34892423+latika18@users.noreply.github.com>
2018-11-13 21:48:22 +0530

    Update Q7.1_deck_of_cards.py

--- Q7.1_deck_of_cards.py
+++ Q7.1_deck_of_cards.py
@@ -1,2 +1,12 @@
 #7.1 Design the data structures for a generic deck of cards.
 #Explain how you would subclass it to implement particular card games. 
+
+class deckofcards(object):
+  def __init__(self):
+    
+    
+   pass
+
+
+
+class diamond(deckofcards):
