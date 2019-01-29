34635c567f581f0e72b3e241322fcb15e41f7159
latika18 <34892423+latika18@users.noreply.github.com>
2019-01-29 23:16:58 +0530

    Update Q030_BFS_on_k-ary_tree.py

--- Q030_BFS_on_k-ary_tree.py
+++ Q030_BFS_on_k-ary_tree.py
@@ -7,3 +7,29 @@ Example input:
 2-> 6
 3-> 7
 6-> 8
+
+class Node(object):
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+class BST(object):
+    def __init__(self, val):
+        self.root = Node(val)
+
+    def insert(self, value):
+        current = self.root
+        while current:
+            if value < current.value:
+                if current.left == None:
+                    current.left = Node(value)
+                    break
+                else:
+                    current = current.left
+            else:
+                if current.right == None:
+                    current.right = Node(value)
+                    break
+                else:
+                    current = current.right
