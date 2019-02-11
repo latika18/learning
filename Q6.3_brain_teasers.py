cc7c1e7d93bc4676c570c16863e88bcfc3fb1d51
latika18 <34892423+latika18@users.noreply.github.com>
2019-02-11 22:03:44 +0530

    Update Q6.3_brain_teasers.py

--- Q6.3_brain_teasers.py
+++ Q6.3_brain_teasers.py
@@ -3,3 +3,11 @@ How would you come up with exactly four quarts of water?
 #NOTE: The jugs are oddly shaped, such that filling up exactly ‘half’ of the jug would be impossible.
 _
 ________________________________________________________________
+5 Quart Contents 3 Quart Contents Note
+5 0 Filled 5 quart jug
+2 3 Filled 3Q with 5Q’s contents
+0 2 Dumped 3Q
+5 2 Filled 5Q
+4 3 Fill remainder of 3Q with 5Q
+4
+Done! We have four quarts
